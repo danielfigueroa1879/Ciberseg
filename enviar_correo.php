9881a49256d66302d36e7c7ab45240f8fa63e6b2
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-05-25 14:31:00 -0400

    Update enviar_correo.php

--- enviar_correo.php
+++ enviar_correo.php
@@ -2,10 +2,11 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
     $email = $_POST["email"];
+    $telefono = $_POST["telefono"];
     $mensaje = $_POST["mensaje"];
     $destinatario = "danielfigueroa1879@gmail.com";
     $asunto = "Nuevo mensaje del formulario de contacto";
-    $cuerpo = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+    $cuerpo = "Nombre: " . $nombre . "\nEmail: " . $email . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
     mail($destinatario, $asunto, $cuerpo);
     echo "¡Mensaje enviado!";
 }
